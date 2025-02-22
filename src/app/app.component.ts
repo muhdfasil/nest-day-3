c5affe29c855cf669bd0eb6cdd00108268776350
muhdfasil <muhammedfasiltk32745@gmail.com>
2025-02-22 20:35:19 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardsComponent } from "./cards/cards.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
